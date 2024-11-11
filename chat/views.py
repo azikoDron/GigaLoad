bbaa755ee66b9dfeaf86d409f3e98f9fca9dda75
Aziz <aziko.mz@gmail.com>
2024-11-11 17:36:57 +0300

    Start html

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    #cards = {'available_apps': Apps.objects.all(), "time_form": DateForm}
+    return render(request, 'chat/index.html')
